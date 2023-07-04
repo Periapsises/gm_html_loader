714fe2cb94f1f69651949be4e5d2b467fd98c55f
Periapsises <94928308+Periapsises@users.noreply.github.com>
2023-07-04 05:56:42 -0400

    Add javascript and css substitution

--- lua/autorun/sh_html_loader.lua
+++ lua/autorun/sh_html_loader.lua
@@ -1,3 +1,5 @@
+local string_len, string_find, string_sub, string_match = string.len, string.find, string.sub, string.match
+
 local htmlFilesLocation = "html/includes/"
 
 local function AddCSLuaFilesRecursive( path )
@@ -16,27 +18,84 @@ end
 
 AddCSLuaFilesRecursive( htmlFilesLocation )
 
-if CLIENT then
-    local patchHook = "InitPostEntity"
-    local patcHookName = "PatchDHTMLPanel"
+------------------------------------------------------------
+-- Client
+if SERVER then return end
 
-    local function patchDHTMLPanel()
-        local DHTML = vgui.GetControlTable( "DHTML" )
+local patchHookName = "InitPostEntity"
+local patchHookIdentifier = "PatchDHTMLPanel"
 
-        function DHTML:OpenFile( fileName )
-            local fullPath = htmlFilesLocation .. fileName .. ".lua"
+local function sourceIsValid( src )
+    return src and not ( string.StartsWith( src, "asset" ) or string.StartsWith( src, "http" ) )
+end
 
-            if not file.Exists( path, "LUA" ) then
-                ErrorNoHalt( "Could not find HTML file: " .. fileName )
-                return
-            end
+local function replaceJavascript( html )
+    local position = 1
+    while position < string_len( html ) do
+        local start, end_ = string_find( html, "<script.-</script>", position )
 
-            local html = file.Read( path, "LUA" )
-            self:SetHTML( html )
+        if not start then break end
+
+        local script = string_sub( html, start, end_ )
+        local src = string_match( script, 'src="([^"]+)"' )
+
+        if sourceIsValid( src ) then
+            local scriptPath = string_match( src, "([^/]+)$" )
+            local scriptContent = "<script>\n" .. file.Read( htmlFilesLocation .. scriptPath .. ".lua", "LUA" ) .. "\n</script>"
+
+            html = string_sub( html, 1, start - 1 ) .. scriptContent .. string_sub( html, end_ + 1 )
+            position = start + string_len( scriptContent )
+        else
+            position = end_ + 1
         end
-
-        hook.Remove( patchHook, patcHookName )
     end
 
-    hook.Add( patchHook, patcHookName, patchDHTMLPanel )
+    return html
 end
+
+local function replaceStyle( html )
+    local position = 1
+    while position < string_len( html ) do
+        local start, end_ = string_find( html, "<link.->", position )
+
+        if not start then break end
+
+        local link = string_sub( html, start, end_ )
+        local href = string_match( link, 'href="([^"]+)"' )
+
+        if sourceIsValid( href ) then
+            local linkPath = string_match( href, "([^/]+)$" )
+            local linkContent = "<style>\n" .. file.Read( htmlFilesLocation .. linkPath .. ".lua", "LUA" ) .. "\n</style>"
+
+            html = string_sub( html, 1, start - 1 ) .. linkContent .. string_sub( html, end_ + 1 )
+            position = start + string_len( linkContent )
+        else
+            position = end_ + 1
+        end
+    end
+
+    return html
+end
+
+local function patchDHTMLPanel()
+    local DHTML = vgui.GetControlTable( "DHTML" )
+
+    function DHTML:OpenFile( fileName )
+        local fullPath = htmlFilesLocation .. fileName .. ".lua"
+
+        if not file.Exists( fullPath, "LUA" ) then
+            ErrorNoHalt( "Could not find HTML file: " .. fileName )
+            return
+        end
+
+        local html = file.Read( fullPath, "LUA" )
+        html = replaceJavascript( html )
+        html = replaceStyle( html )
+
+        self:SetHTML( html )
+    end
+
+    hook.Remove( patchHookName, patchHookIdentifier )
+end
+
+hook.Add( patchHookName, patchHookIdentifier, patchDHTMLPanel )
